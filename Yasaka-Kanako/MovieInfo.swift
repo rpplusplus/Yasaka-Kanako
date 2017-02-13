d4e9919fc1d640550a4207350261d547cc51d6f3
txx <i@rpplusplus.me>
2017-02-13 08:37:25 +0800

    Add Carthagegs

---
+++ Yasaka-Kanako/MovieInfo.swift
@@ -0,0 +1,9 @@
+//
+//  MovieInfo.swift
+//  Yasaka-Kanako
+//
+//  Created by txx on 12/02/2017.
+//  Copyright © 2017 txx. All rights reserved.
+//
+
+import Foundation
---
+++ Yasaka-Kanako/NetworkAdapter.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkAdapter.swift
+//  Yasaka-Kanako
+//
+//  Created by txx on 11/02/2017.
+//  Copyright © 2017 txx. All rights reserved.
+//
+
+import Foundation
